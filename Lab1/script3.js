dd24e113eb5eb1f5966f8e11191c91f341f41c92
AlessandroPenco <alessandro.penco11@gmail.com>
2022-10-27 12:24:24 +0200

    Update script3.js

--- Lab1/script3.js
+++ Lab1/script3.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-const margin = {top: 10, right: 30, bottom: 20, left: 50},
+const margin = {top: 10, right: 30, bottom: 30, left: 40},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
