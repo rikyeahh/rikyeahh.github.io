35ef803dea8acce11292f8b4d60eaceb80b51887
AlessandroPenco <alessandro.penco11@gmail.com>
2022-10-27 12:28:45 +0200

    Update script3.js

--- Lab1/script3.js
+++ Lab1/script3.js
@@ -1,10 +1,5 @@
-// set the dimensions and margins of the graph
-const margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
 // append the svg object to the body of the page
-const svg = d3.select("#graph3")
+const svg3 = d3.select("#graph3")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
