fbef79fcd02c9d62ae3c44f49a467e0a8edc25d9
rikyeahh <riccardororato99@gmail.com>
2022-10-28 11:11:33 +0200

    test on graph4 completed

--- Lab1/script1.js
+++ Lab1/script1.js
@@ -1,48 +1,56 @@
 // set the dimensions and margins of the graph
-const margin = {top: 30, right: 30, bottom: 70, left: 200},
+const margin = { top: 30, right: 30, bottom: 70, left: 200 },
     width = 560 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
+// create box for the svg graph with specified margins and dimentions
 const svg = d3.select("#graph1")
-  .append("svg")
+    .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// Parse the Data
-//d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv").then( function(data) {
-d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/assets/data1.csv").then( function(data) {
 
-data = data.slice(0, 10)
-// X axis
-const x = d3.scaleLinear()
-    .domain([0, Math.max(...data.map(x => x.count))])
-    .range([ 0, width]);
-svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
+// get the data
+d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/assets/data1.csv").then(function (data) {
 
-// Y axis
-const y = d3.scaleBand()
-.range([ 0, height])
-.domain(data.map(d => d.name))
-.padding(.1);
-svg.append("g")
-.call(d3.axisLeft(y))
+    data = data.slice(0, 10) // just top 10 tree species
 
-//Bars
-svg.selectAll("myRect")
-    .data(data)
-    .join("rect")
-    .attr("x", x(0) )
-    .attr("y", d => y(d.name))
-    .attr("width", d => x(d.count))
-    .attr("height", y.bandwidth())
-    .attr("fill", "#69b3a2")
+    // build X axis, linear on quantity
+    const x = d3.scaleLinear()
+        .domain([0, Math.max(...data.map(e => e.count))]) // ticks
+        .range([0, width]); // width on page
+    // put x axis to bottom
+    svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x))
+        // and style its text
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    // build Y axis, adapted on categories
+    const y = d3.scaleBand()
+        .domain(data.map(d => d.name)) // ticks, category names
+        .range([0, height]) // height on page
+        .padding(.1); // separate bars with small padding
+    // put it on the left
+    svg.append("g")
+        .call(d3.axisLeft(y))
+
+    console.log("Y", y.bandwidth())
+    // put bars on graph
+    // build a "myRect" for each datum
+    svg.selectAll("myRect")
+        .data(data)
+        // with shape rect
+        .join("rect")
+        // and dimentions from specified axes
+        .attr("x", x(0)) // x(0) == 0 since start on left side
+        .attr("y", d => y(d.name))
+        .attr("width", d => x(d.count)) // length of horizontal bars
+        .attr("height", y.bandwidth()) // automatic height based on total svg height
+        .attr("fill", "#69b3a2") // color
 
 })
--- Lab1/script4.js
+++ Lab1/script4.js
@@ -1,43 +1,56 @@
-var total = 0;
-var width,
-    height,
-    widthSquares = 10,
-    heightSquares = 10,
-    squareSize = 25,
-    squareValue = 0,
-    gap = 1,
-    theData = [];  
+const margin = { top: 30, right: 0, bottom: 30, left: 50 };
 
-var color = d3.scaleOrdinal(d3.schemeCategory10);
+//Read the data
+d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/assets/data2.csv").then(function (data) {
+    //console.log("INIT DATA", data);
+    toPlot = []
+    Object.entries(data[5])
+        .slice(1) // delete header
+        .forEach(d => toPlot.push({'species': d[0], 'count': d[1]}))
+    //console.log("TO PLOT", toPlot);
+    buildWaffle("graph4", toPlot)
+})
 
-d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/assets/data.csv").then(function(data)
-{
-    //total
-    total = d3.sum(data, function(d) { return d.population; });
-  
+
+function buildWaffle(anchor, data) {
+
+    
+    var total = 0;
+    var width,
+        height,
+        widthSquares = 10,
+        heightSquares = 10,
+        squareSize = 25,
+        squareValue = 0,
+        gap = 1,
+        theData = [];
+
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    total = d3.sum(data, function (d) { return d.count; });
+
     //value of a square
     squareValue = total / (widthSquares * heightSquares);
-    
+
     //remap data
-    data.forEach(function(d, i) 
-    {
-        d.population = +d.population;
-        d.units = Math.floor(d.population/squareValue);
+    data.forEach(function (d, i) {
+        d.count =+ d.count;
+        d.units = Math.floor(d.count / squareValue);
         theData = theData.concat(
-          Array(d.units+1).join(1).split('').map(function()
-            {
-              return {  squareValue:squareValue,                      
-                        units: d.units,
-                        population: d.population,
-                        groupIndex: i};
+            Array(d.units + 1).join(1).split('').map(function () {
+                return {
+                    squareValue: squareValue,
+                    units: d.units,
+                    count: d.count,
+                    groupIndex: i
+                };
             })
-          );
+        );
     });
-  
-    width = (squareSize*widthSquares) + widthSquares*gap + 25;
-    height = (squareSize*heightSquares) + heightSquares*gap + 25;
-  
-    d3.select("#graph4")
+
+    width = (squareSize * widthSquares) + widthSquares * gap + 25;
+    height = (squareSize * heightSquares) + heightSquares * gap + 25;
+
+    var waffle = d3.select("#" + anchor)
         .append("svg")
         .attr("width", width)
         .attr("height", height)
@@ -48,24 +61,52 @@ d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/asset
         .append("rect")
         .attr("width", squareSize)
         .attr("height", squareSize)
-        .attr("fill", function(d)
-        {
-          return color(d.groupIndex);
+        .attr("fill", function (d) {
+            return color(d.groupIndex);
         })
-        .attr("x", function(d, i)
-          {
+        .attr("x", function (d, i) {
             //group n squares for column
-            col = Math.floor(i/heightSquares);
-            return (col*squareSize) + (col*gap);
-          })
-        .attr("y", function(d, i)
-        {
-          row = i%heightSquares;
-          return (heightSquares*squareSize) - ((row*squareSize) + (row*gap))
+            col = Math.floor(i / heightSquares);
+            return (col * squareSize) + (col * gap);
+        })
+        .attr("y", function (d, i) {
+            row = i % heightSquares;
+            return (heightSquares * squareSize) - ((row * squareSize) + (row * gap))
         })
         .append("title")
-          .text(function (d,i) 
-            {
-              return "Age range: " + data[d.groupIndex].age + " | " +  d.population + " , " + d.units + "%"
-            });
-  });
+        .text(function (d, i) {
+            return "Species: " + data[d.groupIndex].species + " | " + d.count + " , " + d.units + "%"
+        });
+
+    //add legend with categorical data
+    var legend = d3.select("#legend")
+        .append("svg")
+        .attr('width', 300)
+        .attr('height', 200)
+        .append('g')
+        .selectAll("div")
+        .data(data)
+        .enter()
+        .append("g")
+        .attr('transform', function (d, i) { return "translate(0," + i * 20 + ")"; });
+    legend.append("rect")
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", function (d, i) { return color(i) });
+    legend.append("text")
+        .attr("x", 25)
+        .attr("y", 13)
+        .text(function (d) { return d.species });
+
+    //add value of a unit square
+    var legend2 = d3.select("#legend")
+        .select('svg')
+        .append('g')
+        .attr('transform', "translate(100,0)");
+
+    legend2.append("text")
+        .attr('y', '14')
+        .attr('font-size', '18px')
+        .text("Total: " + total)
+        .attr("fill", "#444444");
+};
