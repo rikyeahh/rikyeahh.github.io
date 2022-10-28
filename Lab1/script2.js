fe3430d7a88fb3a5c8ec133a2bc4f03ff3927c03
Gagandeep Singh <57759605+50R1Paps@users.noreply.github.com>
2022-10-28 23:53:33 +0200

    Ci siamo quasi

--- Lab1/script2.js
+++ Lab1/script2.js
@@ -13,50 +13,89 @@ d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/asset
     //console.log(circoscrizioni)
 
     const plants = Object.keys(data[0]).filter(d => d != "circoscrizione");
-    console.log(plants.length)
+    //console.log(plants.length)
+    
+    //stack the data? --> stack per subgroup
+    const stackeddata = d3.stack()
+      .keys(plants)(data);
+    //console.log(d3.stack().keys(plants))
+
+  const xMax = d3.max(stackeddata[stackeddata.length - 1], d => d[1]);
 
     // Add X axis
-    const x = d3.scaleBand()
-        .domain(circoscrizioni)
-        .range([0, width])
-    /*svg2.append("g")
+    const x = d3.scaleLinear()
+        .domain([0,xMax]).nice()
+        .range([0, width]);
+    svg2.append("g")
         .attr("transform", `translate(0, ${height})`)
         .call(d3.axisBottom(x).tickSizeOuter(0));
-    */
+    
 
     // Add Y axis
-    const y = d3.scaleLinear()
-        .domain([0, 60])
-        .range([ height, 0 ]);
+    const y = d3.scaleBand()
+        .domain(circoscrizioni)
+        .range([ 0,height])
+        .padding(0.1);
     svg2.append("g")
         .call(d3.axisLeft(y));
 
     // color palette = one color per subgroup
     const color = d3.scaleOrdinal()
         .domain(plants)
-        .range(['#e41a1c','#377eb8','#4daf4a','#6daf7a','#8daf3a','#9daf5b'])
+        .range(d3.schemeTableau10);
 
-    //stack the data? --> stack per subgroup
-    const stackedData = d3.stack()
-        .keys(plants)
-        (data)
-        console.log(stackedData)
-
-
+    const layers = svg2.append('g')
+      .selectAll('g')
+      .data(stackeddata)
+      .join('g')
+      .attr('fill', d => color(d.key));
+   
+    /*svg2.append("g")
+        .selectAll("g")
+        // Enter in the stack data = loop key per key = group per group
+        .data(stackeddata)
+        .join("g")
+          .attr("fill", d => color(d.key))
+          .selectAll("rect")
+          // enter a second time = loop subgroup per subgroup to add all rectangles
+          .data(d => d)
+          .join("rect")
+            .attr("x", function(d) { return x(d); })
+            .attr("y", function(d) { return y(d); })
+            .attr("height", d => y(d.data.circoscrizioni))
+            .attr("width",y.bandwidth())
+            */
+        svg2.append("g")
+            .selectAll("g")
+            // Enter in the stack data = loop key per key = group per group
+            .data(stackeddata)
+            .join("g")
+            .attr("fill", d => color(d.key))
+            .selectAll("rect")
+            // enter a second time = loop subgroup per subgroup to add all rectangles
+            .data(d => d)
+            .join("rect")
+                .attr("x", d => x(d[1]))
+                .attr("y", d => y(d.data.circoscrizione))
+                .attr("height",y.bandwidth())
+                .attr("width", d => x(d[1]) - x(d[0]))
+    /*
     // Show the bars
     svg2.append("g")
         .selectAll("g")
         // Enter in the stack data = loop key per key = group per group
-        .data(stackedData)
+        .data(stackeddata)
         .join("g")
         .attr("fill", d => color(d.key))
         .selectAll("rect")
         // enter a second time = loop subgroup per subgroup to add all rectangles
         .data(d => d)
         .join("rect")
-            .attr("x", d => x(d.data.circoscrizione))
-            .attr("y", d => y(d[1]))
-            .attr("height", d => y(d[0]) - y(d[1]))
-            .attr("width",x.bandwidth())
+            .attr("x", d => y(d[1]))
+            .attr("y", d => x(d.data.circoscrizione))
+            .attr("width", d => x(d[1]) - x(d[0]))
+            .attr("height", y.bandwidth())
+            */
+           
 })
 
