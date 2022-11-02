ee017e2b08d32e8a2d4cbd1f7d5f50aed17b0a22
rikyeahh <riccardororato99@gmail.com>
2022-11-02 11:22:00 +0100

    change color and script

--- Lab1/script2.js
+++ Lab1/script2.js
@@ -1,73 +1,73 @@
-// append the svg object to the body of the page
-const svg2 = d3.select("#graph2")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/assets/data2.csv").then(function (data) {
-    const circoscrizioni = data.map(d => d.circoscrizione);
-    //console.log(circoscrizioni)
+const svg2 = d3.select('#graph2')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
+
+// get the data
+d3.csv("../assets/data2.csv").then(function (data) {
 
     const plants = Object.keys(data[0]).filter(d => d != "circoscrizione");
-    //console.log(plants)
+    const circoscrizioni = data.map(d => d.circoscrizione);
 
-    // stack the data? --> stack per subgroup
-    const stackeddata = d3.stack()
+    const stackedData = d3.stack()
         .keys(plants)(data);
-    //console.log(stackeddata)
 
-    const xMax = d3.max(stackeddata[stackeddata.length - 1], d => d[1]);
-    console.log("xMax", xMax);
-    // add X axis
+    const xMax = d3.max(stackedData[stackedData.length - 1], d => d[1]);
+
+    // scales
     const x = d3.scaleLinear()
         .domain([0, xMax]).nice()
         .range([0, width]);
-    svg2.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x).tickSizeOuter(0));
 
-
-    // add Y axis
     const y = d3.scaleBand()
         .domain(circoscrizioni)
         .range([0, height])
         .padding(0.1);
-    svg2.append("g")
-        .call(d3.axisLeft(y));
+    const color = d3.scaleOrdinal()
+        .domain(plants)
+        .range(d3.schemeTableau10);
 
-    // color palette = one color per subgroup
-    
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
+    // axes
+    const xAxis = d3.axisBottom(x).ticks(5, '~s');
+    const yAxis = d3.axisLeft(y);
+
+    svg2.append('g')
+        .attr('transform', `translate(0,${height})`)
+        .call(xAxis)
+
+    svg2.append("g")
+        .call(yAxis)
+
+    // draw bars
 
     const layers = svg2.append('g')
         .selectAll('g')
-        .data(stackeddata)
+        .data(stackedData)
         .join('g')
         .attr('fill', d => color(d.key));
 
+    // transition for bars
+    const duration = 0;
+    const t = d3.transition()
+        .duration(duration)
+        .ease(d3.easeLinear);
 
-    svg2.append("g")
-        .selectAll("g")
-        // Enter in the stack data = loop key per key = group per group
-        .data(stackeddata)
-        .join("g")
-        .attr("fill", d => {
-            //console.log("d key", d.key);
-            return color(d.key)})
-        .selectAll("rect")
-        // enter a second time = loop subgroup per subgroup to add all rectangles
-        .data(d => d)
-        .join("rect")
-        .attr("x", d => x(d[1]))
-        .attr("y", d => y(d.data.circoscrizione))
-        .attr("height", y.bandwidth())
-        .attr("width", d => {
-            console.log("d", d);
-            console.log(x(d[1]) + '-' + x(d[0]));
-            console.log("x diff ", x(d[1]) - x(d[0]));
-            console.log("----------------")
-            return x(d[1]) - x(d[0])})
+
+    layers.each(function (_, i) {
+
+        d3.select(this)
+            .selectAll('rect')
+            .data(d => d)
+            .join('rect')
+            .attr('x', d => x(d[0]))
+            .attr('y', d => y(d.data.circoscrizione))
+            .attr('height', y.bandwidth())
+            .transition(t)
+            .delay(i * duration)
+            .attr('width', d => (x(d[1]) - x(d[0])));
+    });
+
 })
-
--- Lab1/script3.js
+++ Lab1/script3.js
@@ -1,82 +1,128 @@
-const svg3 = d3.select('#graph3')
-    .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`);
+// Parse the Data
+d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/assets/data2.csv").then(function (data) {
 
-// get the data
-d3.csv("../assets/data2.csv").then(function (data) {
-    //console.log(data[1])
+    //data = data.splice(5,data.length-5)
+    lng = data.length - 5;
+    for (let j = 0; j < lng; j++) {
+        data.pop();
+    }
 
-    const plants = Object.keys(data[0]).filter(d => d != "circoscrizione");
-    //console.log(plants)
-    const circoscrizioni = data.map(d => d.circoscrizione);
-    //console.log(circoscrizioni)
+    const circoscrizioni = data
+        .map(function (val) {
+            return val['circoscrizione'];
+        })
+        .slice(0, 5);
+    circoscrizioni.push("TOTAL")
 
-    const stackedData = d3.stack()
-        .keys(plants)(data);
-    //console.log(d3.stack().keys(plants))
+    const trees = data.columns.slice(1)
+    //console.log("data:", data);
 
-    const xMax = d3.max(stackedData[stackedData.length - 1], d => d[1]);
-    //console.log(stackedData[stackedData.length - 1])
-    //console.log(xMax)
+    const tot = [];
+    for (let j = 0; j < trees.length; j++) {
+        tot[j] = data.map(d => parseInt(d[trees[j]])).reduce((partialSum, a) => partialSum + a, 0);
+    }
 
-    // scales
-    const x = d3.scaleLinear()
-        .domain([0, xMax]).nice()
-        .range([0, width]);
-
-    const y = d3.scaleBand()
-        .domain(circoscrizioni)
-        .range([0, height])
-        .padding(0.1);
-    const color = d3.scaleOrdinal()
-        .domain(plants)
-        .range(d3.schemeTableau10);
-
-    // axes
-    const xAxis = d3.axisBottom(x).ticks(5, '~s');
-    const yAxis = d3.axisLeft(y);
-
-    svg3.append('g')
-        .attr('transform', `translate(0,${height})`)
-        .call(xAxis)
-
-    svg3.append("g")
-        .call(yAxis)
-
-    // draw bars
-
-    // create one group for each fruit
-    const layers = svg3.append('g')
-        .selectAll('g')
-        .data(stackedData)
-        .join('g')
-        .attr('fill', d => color(d.key));
-
-    // transition for bars
-    const duration = 1000;
-    const t = d3.transition()
-        .duration(duration)
-        .ease(d3.easeLinear);
-
-    layers.each(function (_, i) {
-        // this refers to the group for a given fruit
-        d3.select(this)
-            .selectAll('rect')
-            .data(d => d)
-            .join('rect')
-            .attr('x', d => x(d[0]))
-            .attr('y', d => y(d.data.circoscrizione))
-            .attr('height', y.bandwidth())
-            .transition(t)
-            // i is the index of this fruit.
-            // this will give the bars for each fruit a different delay
-            // so that the fruits will be revealed one at a time.
-            // using .each() instead of a normal data join is needed
-            // so that we have access to what fruit each bar belongs to.
-            .delay(i * duration)
-            .attr('width', d => x(d[1]) - x(d[0]));
+    data.push({
+        'circoscrizione': "TOTAL",
+        'Celtis australis': tot[0],
+        'Aesculus hippocastanum': tot[1],
+        'Carpinus betulus': tot[2],
+        'Tilia cordata': tot[3],
+        'Platanus x hispanica': tot[4],
+        'Other': tot[5]
     });
+
+
+    /*console.log("Tot: ", tot);
+    console.log("Circoscrizioni: ", circoscrizioni)
+    console.log("Values: ", values)
+    console.log("Trees: ",trees)*/
+
+    const tooltip3 = d3.select("body")
+        .append("div")
+        .attr("class", "d3-tooltip")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .style("padding", "15px")
+        .style("background", "rgba(0,0,0,0.6)")
+        .style("border-radius", "5px")
+        .style("color", "#fff")
+        .text("a simple tooltip");
+
+    // color palette = one color per subgroup
+    const zip = (a, b) => a.map((k, i) => [k, b[i]]);
+    const color = ["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"];
+    //var color2 = d3.scaleLinear().range(["#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    var color2 = () => "#69b3a2";
+    const y = d3.scaleBand()
+        .domain(trees)
+        .range([height, 0])
+        .padding(.1);
+
+    // Add X axis
+    for (let index = 0; index < circoscrizioni.length; index++) {
+        const svg3 = d3.select("#graph3")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", `translate(${margin.left - 30},${margin.top + 20})`);
+
+        svg3.append("text")
+            .attr("transform", "translate(" + (width) + " ," + (-margin.top) + ")")
+            .style("text-anchor", "middle")
+            .text(circoscrizioni[index])
+
+
+        var values = [];
+        for (let i = 0; i < Object.entries(data[index]).length; i++) {
+            values[i] = Object.entries(data[index])[i][1];
+        }
+
+        values = values.slice(1); // without 'circoscizione'
+        trees_val = zip(trees, values);
+
+        //console.log("Trees/Val: ", trees_val);
+
+        const x = d3.scaleLinear()
+            .domain([0, Math.max(...values)]) //temp written by hand
+            .range([0, width]);
+        svg3.append("g")
+            .call(d3.axisTop(x));
+        //.selectAll("text").remove();    
+
+        svg3.append("g")
+            .call(d3.axisLeft(y));
+
+        svg3.selectAll("myG")
+            .data(trees_val)
+            .join("rect")
+            .attr("x", x)
+            .attr("y", d => y(d[0]))
+            .attr("width", d => x(d[1]))
+            .attr("height", y.bandwidth())
+            .attr("id", function (d, i) {
+                return i;
+            })
+            .attr("fill", function (d, i) {
+                return color2(i);
+            })
+            .on("mouseover", function (d, i) {
+                tooltip3.html(`Count : ${i[1]}`)
+                    .style("visibility", "visible");
+                d3.select(this).attr("fill", "red");
+            })
+            .on("mousemove", function () {
+                tooltip3
+                    .style("top", (event.pageY - 10) + "px")
+                    .style("left", (event.pageX + 10) + "px");
+            })
+            .on("mouseout", function () {
+                tooltip3.html(``).style("visibility", "hidden");
+                d3.select(this).attr("fill", function () {
+                    return "" + color2(this.id) + "";
+                })
+            });
+    }
 })
