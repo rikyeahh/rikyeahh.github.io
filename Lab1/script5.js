3a74cd0ae81044388fc777c1a425b1fedff79053
rikyeahh <riccardororato99@gmail.com>
2022-11-06 13:51:45 +0100

    swapped script5 and test

--- Lab1/script5.js
+++ Lab1/script5.js
@@ -1,92 +1,68 @@
-var total = 0;
-var width,
-    height,
-    widthSquares = 20,
-    heightSquares = 5,
-    squareSize = 25,
-    squareValue = 0,
-    gap = 1,
-    theData = [];  
 
-d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/assets/data2.csv").then(function(data)
-{
-    //console.log(data);
 
-  //total
-  total = d3.sum(data, function(d) { return d.population; });
+d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/assets/data2.csv").then(function (data) {
 
-  //value of a square
-  squareValue = total / (widthSquares*heightSquares);
-  
-  //remap data
-  data.forEach(function(d, i) 
-  {
-      d.population = +d.population;
-      d.units = Math.floor(d.population/squareValue);
-      theData = theData.concat(
-        Array(d.units+1).join(1).split('').map(function()
-          {
-            return {  squareValue:squareValue,                      
-                      units: d.units,
-                      population: d.population,
-                      groupIndex: i};
-          })
-        );
-  });
+    var WIDTH = 750, HEIGHT = 600;
 
-  width = (squareSize*widthSquares) + widthSquares*gap + 25;
-  height = (squareSize*heightSquares) + heightSquares*gap + 25;
+    const margin = { top: 30, right: 30, bottom: 70, left: 200 }
+    width = WIDTH - margin.left - margin.right;
+    height = HEIGHT - margin.top - margin.bottom;
 
-  var waffle = d3.select("#graph5")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .selectAll("div")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("width", squareSize)
-      .attr("height", squareSize)
-      .attr("fill", function(d)
-      {
-        return "red"; //color(d.groupIndex);
-      })
-      .attr("x", function(d, i)
-        {
-          //group n squares for column
-          col = Math.floor(i/heightSquares);
-          return (col*squareSize) + (col*gap);
-        })
-      .attr("y", function(d, i)
-      {
-        row = i%heightSquares;
-        return (heightSquares*squareSize) - ((row*squareSize) + (row*gap))
-      })        
+    widthSquares = 20;
+    heightSquares = 5;
 
-   //add legend with categorical data
-   var legend = d3.select("#legend")
-    .append("svg")
-    .attr('width', 300)
-    .attr('height', 200)
-    .append('g')
-    .selectAll("div")
-    .data(data)
-    .enter()
-      .append("g")
-      .attr('transform', function(d,i){ return "translate(0," + i*20 + ")";});
-  legend.append("rect")
-    .attr("width", 18)
-    .attr("height", 18)
-    .style("fill", function(d, i) { return "red"});
-  legend.append("text")
-    .attr("x", 25)
-    .attr("y", 13)
-    .text( function(d) { return d.circoscrizione});
+    const circoscrizioni = data.map(d => d.circoscrizione);
+    const plants = Object.keys(data[0]).filter(d => d != "circoscrizione");
 
-    //add value of a unit square
-    var legend2 = d3.select("#legend")
-      .select('svg')
-      .append('g')
-      .attr('transform', "translate(100,0)");
+    for (let circoscrizione = 0; circoscrizione < circoscrizioni.length; circoscrizione++) {
+
+        var values = Object.values(data[circoscrizione]).splice(1);
+        //console.log(values);
+        var total = values.reduce((a, b) => parseInt(a) + parseInt(b), 0)
+        values = values.map(v => Math.round(v * 100 / total));
+        //console.log(values, "eccess of", values.reduce((a, b) => a + b, 0) - 100);
+        // fix the eccess/lack due to the rounding by adjusting the "other" class
+        values[values.length - 1] -= values.reduce((a, b) => a + b, 0) - 100;
+        //console.log(values);
+        var sm_margin = 5;
+        var sm_width = 150;
+        var sm_height = 170;
+        var plantColor = d3.schemeTableau10
+        var squareDimension = 10
+        var squarePadding = 2
+
+        const svg5 = d3.select("#graph5")
+            .append("svg")
+            .attr("width", sm_width + sm_margin + 10)
+            .attr("height", sm_height)
+            .append("g")
+            .attr("transform", `translate(${sm_margin},${50})`)
+
+        svg5.append("text")
+            .attr("transform", "translate(" + (sm_width / 2) + " ," + -10 + ")")
+            .style("text-anchor", "middle")
+            .style("font-size", "15")
+            .text(circoscrizioni[circoscrizione])// + " " + values)
+
+
+        var x = 0
+        var y = 0
+        for (let i = 0; i < values.length; i++) {
+            for (let n_tree = 0; n_tree < values[i]; n_tree++) {
+                //console.log("X", x, "Y", y);
+                svg5.append("rect")
+                    .attr("width", squareDimension)
+                    .attr("height", squareDimension)
+                    .attr("x", x)
+                    .attr("y", y)
+                    .attr("transform", `translate(${15},0)`)
+                    .attr("fill", plantColor[i])
+                x += squareDimension + squarePadding
+                if (x != x % ((squareDimension + squarePadding) * 10))
+                    y += squareDimension + squarePadding
+                x %= (squareDimension + squarePadding) * 10
+            }
+
+        }
+    }
 });
