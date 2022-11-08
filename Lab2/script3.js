39d6605775c39e03097428fb2a3029cf4b715682
Gagandeep Singh <57759605+50R1Paps@users.noreply.github.com>
2022-11-08 15:16:05 +0100

    script3.js update
    There is a plot but it should be improved

--- Lab2/script3.js
+++ Lab2/script3.js
@@ -1,24 +1,41 @@
 //Scatter plot
 // append the svg object to the body of the page
-const svg3 = d3.select("#graph3")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          `translate(${margin.left}, ${margin.top})`);
 
 //Read the data
 d3.csv("../assets/data5.csv").then(function(data) {
-  console.log(data)
-  console.log(data[0])
+  
+  //console.log(data)
+  const margin = {top: 10, right: 30, bottom: 30, left: 40},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-  const names_unique = [...new Set(data.map(d => d.name))];
-  console.log(names_unique)
+  const svg3 = d3.select("#graph3")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            `translate(${margin.left}, ${margin.top})`);
+  
+
+  const names = data.map(d => d.name)
+
+
+  //console.log(names_unique)
+
+  const names_ordered = names.sort((a, b) => a.localeCompare(b))
+  //console.log(names_ordered)
+
+  /*var dictionary_names_value = Array.from(new Set(names_ordered)).map(a =>
+    ({name:a, count: names_ordered.filter(f => f === a).length}));*/
+  
+  //console.log(dictionary_names_value);
+  
+
 
   // Add X axis
   const x = d3.scaleLinear()
-    .domain([0, Math.max(... data.map(d => d.height))])
+    .domain([0, 50])
     .range([ 0, width ]);
   svg3.append("g")
     .attr("transform", `translate(0, ${height})`)
@@ -26,14 +43,14 @@ d3.csv("../assets/data5.csv").then(function(data) {
 
   // Add Y axis
   const y = d3.scaleLinear()
-    .domain([0, Math.max(... data.map(d => d.co2_absorption))])
+    .domain([0, 60])
     .range([ height, 0]);
   svg3.append("g")
     .call(d3.axisLeft(y));
 
   // Color scale: give me a specie name, I return a color
   const color = d3.scaleOrdinal()
-    .domain(names_unique)
+    .domain(names_ordered)
     .range(d3.schemeTableau10);
 
 
@@ -42,9 +59,9 @@ d3.csv("../assets/data5.csv").then(function(data) {
     .selectAll("dot")
     .data(data)
     .join("circle")
-      .attr("cx", function (d) { return x(d.height); } )
-      .attr("cy", function (d) { return y(d.Co2_absorpt); } )
-      .attr("r", 5)
+      .attr("cx", function (d) { return x(parseFloat(d.height)); } )
+      .attr("cy", function (d) { return y(parseFloat(d.co2_absorption)); } )
+      .attr("r", 2)
       .style("fill", function (d) { return color(d.name) } )
 
 })
