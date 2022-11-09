839b0739713a041ce9e54e565fe1859223a4225b
AlessandroPenco <alessandro.penco11@gmail.com>
2022-11-09 14:25:23 +0100

    Update script4.js

--- Lab2/script4.js
+++ Lab2/script4.js
@@ -4,18 +4,9 @@
 d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/assets/data5.csv").then(function(data) {
 
     // set the dimensions and margins of the graph
-    const margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 1000,//460 - margin.left - margin.right,
-    height = 2500;//400 - margin.top - margin.bottom;
-
-
-    var svg4 = d3.select("#graph4")
-                .append("svg")
-                    .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                    .attr("transform",
-                        "translate(" + margin.left + "," + margin.top + ")");
+    const margin = {top: 10, right: 20, bottom: 30, left: 30},
+    width = 180,//460 - margin.left - margin.right,
+    height = 180;//400 - margin.top - margin.bottom;
 
     console.log(data);
     const names = data.map(d => d.name);
@@ -34,31 +25,54 @@ d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/asset
     const name_count_ordered = sortable.sort((a, b) => b[1] - a[1]).slice(0,6);
     const names_ordered = name_count_ordered.sort((a, b) => a[0].localeCompare(b[0]));
     
-    console.log(names_ordered);
   // Add X axis
   var x = d3.scaleLinear()
     .domain([0, 50]) //Math.max(...values) of treeSize
     .range([ 0, width ]);
-  svg4.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-
-  // Add Y axis
+    
+    // Add Y axis
   var y = d3.scaleLinear()
     .domain([0, 60]) //Math.max(...values) of CO2
     .range([ height, 0]);
-  svg4.append("g")
-    .call(d3.axisLeft(y));
 
+  const color = d3.scaleOrdinal()
+    .domain(names_ordered)
+    .range(d3.schemeTableau10);
+
+    names_ordered.map(d => d[0]).forEach(e => {
+        const svg4 = d3.select("#graph4")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top*2 + margin.bottom)
+                .append("g")
+                    .attr("transform",
+                    "translate(" + margin.left + "," + margin.top*3 + ")");
+                    
+        svg4.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+        
+        svg4.append("g")
+            .call(d3.axisLeft(y));
+
+
+        svg4.append("text")
+            .attr("transform", "translate(" + (width / 2) + " ," + (-margin.top) + ")")
+            .style("text-anchor", "middle")
+            .text(e)
+
+        svg4.append('g')
+        .selectAll("dot")
+        .data(data.filter(d=>d.name == e))
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) { return x(d.height); } )
+        .attr("cy", function (d) { return y(d.co2_absorption); } )
+        .attr("r", 1.5)
+        .style("fill", function (d) { return color(d.name) } )
+
+    });
   // Add dots
-  svg4.append('g')
-    .selectAll("dot")
-    .data(data)
-    .enter()
-    .append("circle")
-      .attr("cx", function (d) { return x(d.GrLivArea); } )
-      .attr("cy", function (d) { return y(d.SalePrice); } )
-      .attr("r", 1.5)
-      .style("fill", "#69b3a2")
+  
 
 })
