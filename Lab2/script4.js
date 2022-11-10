bcf15418b1951916b6bed24cc2e3aec8ae760c86
Alessandro Arata <alessandro.arata99@gmail.com>
2022-11-10 12:20:42 +0100

    Added regression line to small multiples

--- Lab2/script4.js
+++ Lab2/script4.js
@@ -5,8 +5,8 @@ d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/asset
 
     // set the dimensions and margins of the graph
     const margin = {top: 10, right: 20, bottom: 30, left: 30},
-    width = 180,//460 - margin.left - margin.right,
-    height = 180;//400 - margin.top - margin.bottom;
+    width = 300,//460 - margin.left - margin.right,
+    height = 300;//400 - margin.top - margin.bottom;
 
     console.log(data);
     const names = data.map(d => d.name);
@@ -27,27 +27,28 @@ d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/asset
     
   // Add X axis
   var x = d3.scaleLinear()
-    .domain([0, 50]) //Math.max(...values) of treeSize
+    .domain([0, Math.max(...data.map(e => parseFloat(e.height)))]) //Math.max(...values) of treeSize
     .range([ 0, width ]);
     
     // Add Y axis
   var y = d3.scaleLinear()
-    .domain([0, 60]) //Math.max(...values) of CO2
+    .domain([0, Math.max(...data.map(e => parseFloat(e.co2_absorption)))]) //Math.max(...values) of CO2
     .range([ height, 0]);
 
   const color = d3.scaleOrdinal()
     .domain(names_ordered)
     .range(d3.schemeTableau10);
-
+    
     names_ordered.map(d => d[0]).forEach(e => {
         const svg4 = d3.select("#graph4")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top*2 + margin.bottom)
+                    .attr("height", height + margin.top*2 + margin.bottom*2)
                 .append("g")
                     .attr("transform",
-                    "translate(" + margin.left + "," + margin.top*3 + ")");
-                    
+                    "translate(" + margin.left*2+ "," + margin.top*3 + ")")
+        .style("margin-bottom", "50px");
+
         svg4.append("g")
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x));
@@ -62,17 +63,69 @@ d3.csv("https://raw.githubusercontent.com/rikyeahh/rikyeahh.github.io/main/asset
             .text(e)
 
         svg4.append('g')
-        .selectAll("dot")
-        .data(data.filter(d=>d.name == e))
-        .enter()
-        .append("circle")
-        .attr("cx", function (d) { return x(d.height); } )
-        .attr("cy", function (d) { return y(d.co2_absorption); } )
-        .attr("r", 1.5)
-        .style("fill", function (d) { return color(d.name) } )
+          .selectAll("dot")
+          .data(data.filter(d=>d.name == e))
+          .enter()
+          .append("circle")
+          .attr("cx", function (d) { return x(d.height); } )
+          .attr("cy", function (d) { return y(d.co2_absorption); } )
+          .attr("r", 1.5)
+          .style("fill", function (d) { return color(d.name) } )
+          
+            svg4.append("text")
+            .attr("text-anchor", "end")
+            .attr("x", (width / 2) + 50)
+            .attr("y", height + 35)
+            .text("Tree height (m)");
 
+          svg4.append("text")
+            .attr("text-anchor", "end")
+            .attr("x", (width / 2) - 230)
+            .attr("y", height - 330)
+            .text("CO2 absorption (kg/year)")
+            .attr("transform", "rotate(-90)");
+          
+
+          var yval = data.filter(d=>d.name == e).map(function (d) { return parseFloat(d.co2_absorption); });
+          var xval = data.filter(d=>d.name == e).map(function (d) { return parseFloat(d.height); });
+        
+          var lr = linearRegression(yval, xval);
+
+          var max = d3.max(data, function (d) { return parseFloat(d.height); });
+          
+          svg4.append("svg:line")
+            .attr("x1", x(0))
+            .attr("y1", y(lr.intercept))
+            .attr("x2", x(max))
+            .attr("y2", y((max * lr.slope) + lr.intercept))
+            .style("stroke", "gray")
+            .style("opacity", 0.5);
     });
-  // Add dots
-  
 
+    function linearRegression(y,x){
+
+      var lr = {};
+      var n = y.length;
+      var sum_x = 0;
+      var sum_y = 0;
+      var sum_xy = 0;
+      var sum_xx = 0;
+      var sum_yy = 0;
+
+      for (var i = 0; i < y.length; i++) {
+
+          sum_x += x[i];
+          sum_y += y[i];
+          sum_xy += (x[i]*y[i]);
+          sum_xx += (x[i]*x[i]);
+          sum_yy += (y[i]*y[i]);
+      } 
+
+      lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n*sum_xx - sum_x * sum_x);
+      lr['intercept'] = (sum_y - lr.slope * sum_x)/n;
+      lr['r2'] = Math.pow((n*sum_xy - sum_x*sum_y)/Math.sqrt((n*sum_xx-sum_x*sum_x)*(n*sum_yy-sum_y*sum_y)),2);
+
+      return lr;
+
+};
 })
