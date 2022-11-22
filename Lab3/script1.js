b17226017c3c8d3fcb029ca0e03a427bbc677772
rikyeahh <riccardororato99@gmail.com>
2022-11-22 14:08:35 +0100

    map1

--- Lab3/script1.js
+++ Lab3/script1.js
@@ -1,51 +1,116 @@
 // Choropleth Map - Absolute tree count
 
 // Data and color scale
+const data = new Map();
 
-let data1 = new Map()
 // Load external data and boot
 Promise.all([
-    //d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"),
     d3.json("../assets/circoscrizioni.json"),
-    //d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function (d) {
     d3.csv("../assets/data7.csv", function (d) {
-        data1.set(d.code, d.pop)
-        if(d.name == 'Zambia'){
-            console.log(d)
-        }
+        console.log(d.name);
+        data.set(d.name, +d.pop)
     })
-]).then(function (loadData) {
-    //console.log("data", data1);
-    //loadData = loadData[0]
-    console.log("loadData", loadData);
-    
-    const svg = d3.select("#svg1"),
-        width = + svg.attr("width"),
-        height = + svg.attr("height");
+])
+    .then(function (loadData) {
 
-    // Map and projection
-    const projection = d3.geoMercator()
-        .scale(40)
-        .center([0, 20])
-        .translate([width / 2, height / 2]);
-    const colorScale = d3.scaleThreshold()
-        .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
-        .range(d3.schemeBlues[7]);
-    let topo = loadData[0]
-    console.log("loadData[0]", topo.features);
+        // Set the dimensions and margins of the graph
+        const margin_1 = { top: 50, right: 20, bottom: 70, left: 20 },
+            width_1 = 1024 - margin_1.left - margin_1.right,
+            height_1 = 768 - margin_1.top - margin_1.bottom;
 
-    // Draw the map
-    svg.append("g")
-        .selectAll("path")
-        .data(topo.features)
-        .join("path")
-        // draw each country
-        .attr("d", d3.geoPath()
-            .projection(projection)
-        )
-        // set the color of each country
-        .attr("fill", function (d) {
-            d.total = data1.get(d.properties.nome) || 0;
-            return colorScale(d.total);
-        })
-})
+        // Append the svg_1 object to the page
+        const svg_1 = d3.select("#graph1")
+            .append("svg")
+            //.attr("width", width_1 + margin_1.left + margin_1.right)
+            //.attr("height", height_1 + margin_1.top + margin_1.bottom)
+            .attr("viewBox", '0 0 ' + (width_1 + margin_1.left + margin_1.right) +
+                ' ' + (height_1 + margin_1.top + margin_1.bottom))
+            .append("g")
+            .attr("transform", `translate(${margin_1.left}, ${margin_1.top})`);
+
+        // Map and projection
+        var projection = d3.geoIdentity().reflectY(true)
+
+        const colorScale = d3.scaleThreshold()
+            .domain([10, 100, 300, 500, 1000, 2000, 3000])
+            .range(d3.schemeGreens[8]);
+
+        const tooltip_1 = d3.select("#graph1")
+            .append("div")
+            .attr("class", "tooltip")
+            .style("font-size", "14px")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "1px")
+            .style("border-radius", "5px")
+            .style("padding", "10px")
+            .style("opacity", 0);
+        let topo = loadData[0]
+        projection.fitSize([width_1, height_1], topo)
+
+        // Draw the map
+        svg_1.append("g")
+            .selectAll("path")
+            .data(topo.features)
+            .join("path")
+            // draw each country
+            .attr("d", d3.geoPath()
+                .projection(projection)
+            )
+            // set the color of each country
+            .attr("fill", (d) => { 
+                console.log(d.properties.nome);
+                return colorScale(data.get(d.properties.nome)) }
+            )
+            .style("fill-opacity", "0.9")
+            .attr("class", (d) => `circo${d.properties.numero_cir}`)
+            .style("stroke", "white")
+
+        svg_1.join("g")
+            .selectAll("path")
+            .on("mouseover", function (event, d) {
+                // Select all the rect with this specific class (tree species)
+                var circo = d.properties.numero_cir;
+                svg_1.selectAll("path")
+                    .style("stroke", "transparent")
+                    .style("fill-opacity", "0.5")
+                    .transition("selected")
+                    .duration(300)
+
+                svg_1.selectAll(`.circo${circo}`)
+                    .style("fill-opacity", "1")
+                    .transition("selected")
+                    .duration(300)
+                    .style("stroke", "#000")
+                    .style("stroke-width", "2px")
+
+                tooltip_1.transition("appear-box")
+                    .duration(300)
+                    .style("opacity", .9)
+                    .delay(1);
+
+                console.log(d)
+
+                // ---------------- TODO --------------------
+                // Aggiungere l'area corretta nel tooltip
+                tooltip_1.html("<span class='tooltiptext'>" + "<b>Name: " + d.properties.nome + "</b><br>Tree Abundance: " + data.get(d.properties.nome) +
+                    "</span>")
+                    .style("left", (event.pageX) + "px")
+                    .style("top", (event.pageY - 28) + "px");
+            })
+            .on("mouseout", function (event, d) {
+                // Select all the rect with this specific class (tree species)
+
+                svg_1.selectAll("path")
+                    .style("stroke", "white")
+                    .style("stroke-width", "1")
+                    .style("fill-opacity", "0.9")
+                    .transition("selected")
+                    .duration(300)
+
+                tooltip_1.transition("disappear-box")
+                    .duration(300)
+                    .style("opacity", 0);
+            });
+
+    })
