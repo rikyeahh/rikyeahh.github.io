dd6a3844cfd40e169e14787fd78340062fd195b9
rikyeahh <riccardororato99@gmail.com>
2022-12-10 10:23:25 +0100

    added year selection in graph 3

--- Lab4/script3.js
+++ Lab4/script3.js
@@ -1,107 +1,112 @@
 // ridgeline
 
+function redrawRidgeline(years) {
+    d3.csv("../assets/data10.csv").then(data => {
 
-d3.csv("../assets/data10.csv").then(data => {
+        // take data only of the specified years
+        data = data.filter(line => years.includes(line.year.slice(0, 4)))
 
-    dataMax = data.filter(line => line.year.slice(-3) == "max")
-    dataMin = data.filter(line => line.year.slice(-3) == "min")
+        // max and min temperatures of each month of years
+        dataMax = data.filter(line => line.year.slice(-3) == "max")
+        dataMin = data.filter(line => line.year.slice(-3) == "min")
     
-    dataMax = preprocessData(dataMax);
-    dataMin = preprocessData(dataMin);
-
-    // set the dimensions and margins of the graph
-    const margin = { top: 60, right: 30, bottom: 20, left: 110 },
-        width = 500 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-
-    // append the svg object to the body of the page
-    const svg = d3.select("#graph3")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-            `translate(${margin.left}, ${margin.top})`);
-
-    // Get the different categories and count them
-    const categories = dataMax.columns
-    const n = categories.length
-
-    // Add X axis
-    const x = d3.scaleLinear()
-        .domain([-20, 45])
-        .range([0, width]);
-    svg.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x));
-
-    // Create a Y scale for densities
-    const y = d3.scaleLinear()
-        .domain([0, 0.4])
-        .range([height, 0]);
-
-    // Create the Y axis for names
-    const yName = d3.scaleBand()
-        .domain(categories)
-        .range([0, height])
-        .paddingInner(1)
-    svg.append("g")
-        .call(d3.axisLeft(yName));
-
-    // Compute kernel density estimation for each column:
-    var kde = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(40)) // increase this 40 for more accurate density.
-    var allDensity = []
-    for (i = 0; i < n; i++) {
-        key = categories[i]
-        density = kde(dataMax.map(function (d) { return d[key]; }))
-        allDensity.push({ key: key, density: density })
-    }
-
-    // Add areas
-    svg.selectAll("areas")
-        .data(allDensity)
-        .join("path")
-        .attr("transform", function (d) { return (`translate(0, ${(yName(d.key) - height)})`) })
-        .datum(function (d) { return (d.density) })
-        .attr("fill", "#ffffff")
-        .attr("fill-opacity","0")
-        .attr("stroke", "#c91e24")
-        .attr("stroke-width", 2)
-        .attr("d", d3.line()
-            .curve(d3.curveBasis)
-            .x(function (d) { return x(d[0]); })
-            .y(function (d) { return y(d[1]); })
-        )
-
-            // Compute kernel density estimation for each column:
-    kde = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(40)) // increase this 40 for more accurate density.
-    allDensity = []
-    for (i = 0; i < n; i++) {
-        key = categories[i]
-        density = kde(dataMin.map(function (d) { return d[key]; }))
-        allDensity.push({ key: key, density: density })
-    }
-
-    // Add areas
-    svg.selectAll("areas")
-        .data(allDensity)
-        .join("path")
-        .attr("transform", function (d) { return (`translate(0, ${(yName(d.key) - height)})`) })
-        .datum(function (d) { return (d.density) })
-        .attr("fill", "#ffffff")
-        .attr("fill-opacity","0")
-        .attr("stroke", "#1f19c2")
-        .attr("stroke-width", 2)
-        .attr("d", d3.line()
-            .curve(d3.curveBasis)
-            .x(function (d) { return x(d[0]); })
-            .y(function (d) { return y(d[1]); })
-        )
-
-})
-
-function drawLine() {
+        dataMax = preprocessData(dataMax);
+        dataMin = preprocessData(dataMin);
     
+        // set the dimensions and margins of the graph
+        const margin = { top: 60, right: 30, bottom: 20, left: 110 },
+            width = 500 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+    
+        // clear the previous graph viz
+        document.getElementById("graph3").innerHTML = '';
+
+        // append the svg object to the body of the page
+        const svg = d3.select("#graph3")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform",
+                `translate(${margin.left}, ${margin.top})`);
+    
+        // Get the different categories and count them
+        const categories = dataMax.columns
+        const n = categories.length
+    
+        // Add X axis
+        const x = d3.scaleLinear()
+            .domain([-20, 45])
+            .range([0, width]);
+        svg.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(x));
+    
+        // Create a Y scale for densities
+        const y = d3.scaleLinear()
+            .domain([0, 0.4])
+            .range([height, 0]);
+    
+        // Create the Y axis for names
+        const yName = d3.scaleBand()
+            .domain(categories)
+            .range([0, height])
+            .paddingInner(1)
+        svg.append("g")
+            .call(d3.axisLeft(yName));
+    
+        // Compute kernel density estimation for each month (dataMax):
+        var kde = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(40)) // increase this 40 for more accurate density.
+        var allDensity = []
+        for (i = 0; i < n; i++) {
+            key = categories[i]
+            density = kde(dataMax.map(function (d) { return d[key]; }))
+            allDensity.push({ key: key, density: density })
+        }
+    
+        // Add areas
+        svg.selectAll("areas")
+            .data(allDensity)
+            .join("path")
+            .attr("transform", function (d) { return (`translate(0, ${(yName(d.key) - height)})`) })
+            .datum(function (d) { return (d.density) })
+            .attr("fill", "#ffffff")
+            .attr("fill-opacity", "0")
+            .attr("stroke", "#c91e24")
+            .attr("stroke-width", 2)
+            .attr("d", d3.line()
+                .curve(d3.curveBasis)
+                .x(function (d) { return x(d[0]); })
+                .y(function (d) { return y(d[1]); })
+            )
+    
+        // Compute kernel density estimation for each month (dataMin):
+        kde = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(40)) // increase this 40 for more accurate density.
+        allDensity = []
+        for (i = 0; i < n; i++) {
+            key = categories[i]
+            density = kde(dataMin.map(function (d) { return d[key]; }))
+            allDensity.push({ key: key, density: density })
+        }
+    
+        // Add areas
+        svg.selectAll("areas")
+            .data(allDensity)
+            .join("path")
+            .attr("transform", function (d) { return (`translate(0, ${(yName(d.key) - height)})`) })
+            .datum(function (d) { return (d.density) })
+            .attr("fill", "#ffffff")
+            .attr("fill-opacity", "0")
+            .attr("stroke", "#1f19c2")
+            .attr("stroke-width", 2)
+            .attr("d", d3.line()
+                .curve(d3.curveBasis)
+                .x(function (d) { return x(d[0]); })
+                .y(function (d) { return y(d[1]); })
+            )
+    
+    })
+
 }
 
 // This is what I need to compute kernel density estimation
@@ -119,8 +124,7 @@ function kernelEpanechnikov(k) {
 }
 
 function preprocessData(dataMax) {
-    const sumstat = d3.group(dataMax, d => d.month);
-    var arrayData = [[],[],[],[],[],[],[],[],[],[],[],[]];
+    var arrayData = [[], [], [], [], [], [], [], [], [], [], [], []];
     dataMax.forEach(element => {
         arrayData[parseInt(element.month) - 1].push(element.temp)
     });
@@ -145,3 +149,25 @@ function preprocessData(dataMax) {
     finalData.columns = ['JAN', 'FEB', 'MAR', 'APR', 'APR', 'MAY', 'JUN', 'JUL', 'SEP', 'OCT', 'NOV', 'DEC']
     return finalData
 }
+
+
+// on year selection and submit, redraw the graph
+const form = document.querySelector("form");
+
+form.addEventListener(
+  "submit",
+  (event) => {
+    const data = new FormData(form);
+    let chosenYears = [];
+    for (const entry of data) {
+        chosenYears.push(entry[1]);
+    }
+    console.log(chosenYears);
+    redrawRidgeline(chosenYears);
+    event.preventDefault();
+  },
+  false
+);
+
+// by default, show all years
+redrawRidgeline(['1993', '1997', '2001', '2005', '2009', '2013', '2017', '2021']);
